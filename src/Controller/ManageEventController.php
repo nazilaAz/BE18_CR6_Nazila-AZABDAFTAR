efffd55ee457a8eab3d98ee2f5bec8ace4210a6e
Nazila <azabdaftar@outlook.com>
2023-04-14 21:16:16 +0200

    Details page

--- src/Controller/ManageEventController.php
+++ src/Controller/ManageEventController.php
@@ -2,17 +2,31 @@
 
 namespace App\Controller;
 
+use App\Entity\Event;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ManageEventController extends AbstractController
 {
-    #[Route('/manage/event', name: 'app_manage_event')]
-    public function index(): Response
+    private $em;
+    public function __construct(EntityManagerInterface $em)
     {
-        return $this->render('manage_event/index.html.twig', [
-            'controller_name' => 'ManageEventController',
+        $this->em = $em;
+    }
+
+    #[Route('/manage_event/show/{id}', name: 'app-show-details')]
+    public function details($id): Response
+    {
+        $event = $this->em->getRepository(Event::class)->find($id);
+
+        // Foreign-Key call getter from Controller
+        // $status = $event->getFkStatus();
+
+        return $this->render('manage_event/show.html.twig', [
+            'event' => $event,
+            // 'Status' => $status
         ]);
     }
 }
